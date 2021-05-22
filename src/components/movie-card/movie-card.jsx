3baf1ba15955ce2bfe5a353a41a12a91dc235d7c
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-05-22 13:32:53 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { Card } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
-    // This is given to the <MovieCard/> component by the outer world
-    // which, in this case, is `MainView`, as `MainView` is what’s
-    // connected to your database via the movies endpoint of your API
-    const { movie, onClick } = this.props;
-
+    const { movie, onMovieClick } = this.props;
     return (
-      <Card
-        onClick={() => onClick(movie)}
-        border='danger'
-        style={{ width: '200', height: 'auto' }}
-      >
-        <Card.Header>{movie.title}</Card.Header>
-        <img
-          className='movie-poster'
-          src={movie.imagePath}
-          alt='movie poster'
-        />
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant
+            ="link">Open</Button>
+        </Card.Body>
       </Card>
     );
   }
@@ -29,19 +22,9 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    imagePath: PropTypes.string.isRequired,
-    genre: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    }),
-    director: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      bio: PropTypes.string.isRequired,
-      birth: PropTypes.string.isRequired,
-      death: PropTypes.string,
-    }),
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
   }).isRequired,
-  onClick: PropTypes.func.isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
