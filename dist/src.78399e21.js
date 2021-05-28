1c6c59e1af46b82b9b7261447e9896a65d12be0e
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-05-28 22:50:54 +0100

    Changes

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -34897,7 +34897,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< Updated upstream
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "8721" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12413" + '/');
+>>>>>>> Stashed changes
 
   ws.onmessage = function (event) {
     checkedAssets = {};
