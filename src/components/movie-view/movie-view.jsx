409c697327aa3c156cacb65a952ab220576dc991
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-05-28 21:17:53 +0100

    Big update

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,39 +1,95 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+
+import axios from 'axios'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './movie-view.scss'
+
+import { Navigation } from '../Navigation/navigation'
 
 export class MovieView extends React.Component {
+  constructor() {
+    super()
 
-  keypressCallback(event) {
-    console.log(event.key);
+    this.state = {
+      movies: [],
+      favoriteMovies: []
+    }
   }
 
-  componentDidMount() {
-    document.addEventListener('keypress', this.keypressCallback);
-  }
+  addToFavoriteMovies(movie) {
+    const username = localStorage.getItem('user')
+    const token = localStorage.getItem('token')
 
-  componentWillUnmount() {
-    document.removeEventListener('keypress', this.keypressCallback);
+    axios
+      .post(
+        `https://flixofficial.herokuapp.com//users/${username}/Movies/${movie}`,
+        {
+          FavoriteMovies: this.FavoriteMovies
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+      )
+      .then(response => {
+        this.setState({
+          FavoriteMovies: response.data.FavoriteMovies
+        })
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    alert('movie successfully added.')
   }
 
   render() {
-    const { movie, onBackClick } = this.props;
+    const { movie } = this.props
+
+    if (!movie) return null
 
     return (
-      <div className='movie-view'>
-        <div className='movie-poster'>
-          <img src={movie.ImagePath} />
-        </div>
-        <div className='title'>
-          <span className='label'>Title: </span>
-          <span className='value'>{movie.Title}</span>
-        </div>
-        <div className='description'>
-          <span className='label'>Description: </span>
-          <span className='value'>{movie.Description}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
+      <div className='view-container'>
+        <Navigation />
+        <Container className='movie-view-container'>
+          <div className='movie-view'>
+            <img className='movie-poster' variant='top' src={movie.ImagePath} />
+            <div className='movie-title'>
+              <span className='label'><b>Title: </b></span>
+              <span className='value'>{movie.Title}</span>
+            </div>
+            <div className='movie-description'>
+              <span className='label'><b>Description: </b></span>
+              <span className='value'> {movie.Description}</span>
+            </div>
+
+            <div className='movie-genre'>
+              <span className='label'> <b>Genre: </b></span>
+              <span className='value'>{movie.Genre.Name}</span>
+            </div>
+            <div className='movie-director'>
+              <span className='label'><b>Director: </b></span>
+              <span className='value'>{movie.Director.Name}</span>
+            </div>
+            <Link to={`/Directors/${movie.Director.Name}`}>
+              <Button className='button-director'>Director</Button>
+            </Link>
+            <Link to={`/`}>
+              <Button className=' button-goBack'>Back</Button>
+            </Link>
+            <Link to={`/genres/${movie.Genre.Name}`}>
+              <Button className='button-genre'>{movie.Genre.Name}</Button>
+            </Link>
+            <Button
+              className='button-favorite'
+              onClick={() => this.addToFavoriteMovies(movie._id)}
+            >
+              Add to Favorites
+            </Button>
+          </div>
+        </Container>
       </div>
-    );
+    )
   }
 }
 
@@ -41,17 +97,16 @@ MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
     }),
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string,
+      Birth: PropTypes.date,
+      Death: PropTypes.date
     }),
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired
-};
+    ImagePath: PropTypes.string.isRequired
+  })
+}
