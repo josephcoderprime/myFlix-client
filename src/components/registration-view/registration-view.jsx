905d179b72041ddf52b1e58056ed6f679d01994b
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-05-28 21:11:15 +0100

    Dist

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,55 +1,133 @@
 import React, { useState } from 'react';
 import Proptypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import "./registration-view.scss";
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [birthdate, setBirthdate] = useState('');
+  const [birthday, setBirthday] = useState('');
 
-  const handleSubmit = (e) => {
+  const [usernameError, setUsernameError] = useState({});
+  const [passwordError, setPasswordError] = useState({});
+  const [emailError, setEmailError] = useState({});
+
+
+  const handleRegister = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthdate);
-    props.onRegister(username);
+    console.log(username, password, email, birthday);
+    const isValid = formValidation();
+    if (isValid) {
+      axios.post('https://flixofficial.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      }).then(response => {
+        const data = response.data;
+        console.log(data);
+      }).catch(e => {
+        console.log('error in registering the user')
+        alert('Error')
+      });
+    }
   }
+
+  const formValidation = () => {
+    const usernameError = {};
+    const passwordError = {};
+    const emailError = {};
+    let isValid = true;
+
+    if (username.trim().length < 5) {
+      usernameError.usernameShort = "Username must be at least 5 characters";
+      isValid = false;
+    }
+
+    if (password.trim().length < 1) {
+      passwordError.passwordMissing = "You must enter a password";
+      isValid = false;
+    }
+
+    if (!email.includes(".") && !email.includes("@")) {
+      emailError.emailNotEmail = "A valid email address is required";
+      isValid = false;
+    }
+
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+    setEmailError(emailError);
+    return isValid;
+  };
 
   return (
     <Form>
-      <Form.Group controlId='registerUsername'>
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type='text' onChange={e => setUsername(e.target.value)} />
+      <Form.Group controlId="registerUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
       </Form.Group>
-      <Form.Group controlId='registerPassword'>
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type='password' onChange={e => setPassword(e.target.value)} />
+      {Object.keys(usernameError).map((key) => {
+        return (
+          <div key={key} style={{ color: "red" }}>
+            {usernameError[key]}
+          </div>
+        );
+      })}
+
+      <Form.Group controlId="registerPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
       </Form.Group>
-      <Form.Group controlId='registerEmail'>
-        <Form.Label>Email:</Form.Label>
-        <Form.Control type='text' onChange={e => setEmail(e.target.value)} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-    </Form.Text>
+
+      {Object.keys(passwordError).map((key) => {
+        return (
+          <div key={key} style={{ color: "red" }}>
+            {usernameError[key]}
+          </div>
+        );
+      })}
+
+      <Form.Group controlId="registerEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
       </Form.Group>
-      <Form.Group controlId='registerBirthdate'>
-        <Form.Label>Birthdate:</Form.Label>
-        <Form.Control type='text' onChange={e => setBirthdate(e.target.value)} />
+      {Object.keys(emailError).map((key) => {
+        return (
+          <div key={key} style={{ color: "red" }}>
+            {emailError[key]}
+          </div>
+        );
+      })}
+
+      <Form.Group controlId="registerBirthday">
+        <Form.Label>Birthdate</Form.Label>
+        <Form.Control type="text" onChange={e => setBirthday(e.target.value)} />
       </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handleRegister}>Register</Button>
       <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Check type="checkbox" label="Remember me" />
       </Form.Group>
-      <Button variant='primary' type='submit' onClick={handleSubmit}>
-        Submit
-            </Button>
-      <Button variant='secondary' onClick={props.toggleRegister}>
-        Existing User
-            </Button>
+      <Link to={`/`}>
+        <Button variant="outline-primary">Already Registered? Log In</Button>
+      </Link>
+
 
     </Form>
-  )
+  );
 }
 
 RegistrationView.Proptypes = {
-  onRegister: Proptypes.func.isRequired
+  register: Proptypes.shape({
+    Username: Proptypes.string,
+    Password: Proptypes.string,
+    Email: Proptypes.string,
+    Birthday: Proptypes.string
+  }),
+  onRegister: Proptypes.func
 };
