654dce40ec60bcaf8af2d6b77dc2389572d160c9
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-05-28 21:18:43 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,60 +1,102 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import Proptypes from 'prop-types';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
+
+import "./login-view.scss";
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const [usernameError, setUsernameError] = useState({});
+    const [passwordError, setPasswordError] = useState({});
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        /* Send a request to the server for authentication */
-        axios.post('https://flixofficial.herokuapp.com/login', {
-            Username: username,
-            Password: password
-        })
-            .then(response => {
+        console.log(username, password);
+        const isValid = formValidation();
+        if (isValid) {
+            axios.post('https://flixofficial.herokuapp.com/login', {
+                Username: username,
+                Password: password
+            }).then(response => {
                 const data = response.data;
                 props.onLoggedIn(data);
-            })
-            .catch(e => {
-                console.log('The user doesnt exist')
-                alert('Invalid username or password');
+            }).catch(e => {
+                console.log('no such user')
             });
+        }
     };
 
+    const formValidation = () => {
+        const usernameError = {};
+        const passwordError = {};
+        let isValid = true;
+
+        if (username.trim().length < 5) {
+            usernameError.usernameShort = "Username must be at least 5 characters";
+            isValid = false;
+        }
+
+        if (password.trim().length < 1) {
+            passwordError.passwordMissing = "You must enter a password";
+            isValid = false;
+        }
+
+        setUsernameError(usernameError);
+        setPasswordError(passwordError);
+        return isValid;
+    };
     return (
-        <Form>
-            <h1 className='text'>Welcome to myFlix!</h1>
-            <p className='mb-5'>Please login to continue.</p>
-            <Form.Group controlId='formUsername'>
-                <Form.Label>Username:</Form.Label>
-                <Form.Control type='text' placeholder='Enter Username' onChange={e => setUsername(e.target.value)} />
-            </Form.Group>
+        <div>
+            <Form>
+                <h1 className='text'>Welcome to myFlix!</h1>
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder='Enter Username' onChange={e => setUsername(e.target.value)} />
+                </Form.Group>
+                {Object.keys(usernameError).map((key) => {
+                    return (
+                        <div key={key} style={{ color: "red" }}>
+                            {usernameError[key]}
+                        </div>
+                    );
+                })}
 
-            <Form.Group controlId='formPassword'>
-                <Form.Label>Password:</Form.Label>
-                <Form.Control type='password' placeholder='Enter Password' onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder='Enter Password' onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                {Object.keys(passwordError).map((key) => {
+                    return (
+                        <div key={key} style={{ color: "red" }}>
+                            {usernameError[key]}
+                        </div>
+                    );
+                })}
 
-            <Button variant='success' type='submit' onClick={handleSubmit}>
-                Login
-            </Button>
-            <Button variant='dark' onClick={props.toggleRegister}>
-                New User
-            </Button>
-        </Form>
+                <Button variant="success" type="submit" onClick={handleSubmit}>Login</Button>
+                <Link to={`/register`}>
+                    <Button variant="outline-primary" >Not Registered? Register</Button>
+                </Link>
+            </Form>
+            <p><b>For testing.</b><br />
+                <b> Username:</b> Myflixofficial <br />
+                <b>Password:</b> 13432
+       </p>
+        </div>
+
     );
 }
 
-LoginView.propTypes = {
-    user: PropTypes.shape({
-        username: PropTypes.string.isRequired,
-        password: PropTypes.string.isRequired,
+LoginView.Proptypes = {
+    user: Proptypes.shape({
+        Username: Proptypes.string,
+        Password: Proptypes.string
     }),
-    onLoggedIn: PropTypes.func.isRequired,
-    onRegister: PropTypes.func,
+    onLoggedIn: Proptypes.func
 };
