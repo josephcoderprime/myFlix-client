32c864ff7beddc64567678056baad07142d6abf9
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-05-29 12:38:06 +0100

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -52739,7 +52739,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14526" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "17519" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
