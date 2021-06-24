8667127a0cd93aca95fbc5359f851473e9aa6045
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-24 21:26:55 +0100

    Update movies-list.jsx

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Container } from 'react-bootstrap';
 
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
@@ -14,15 +15,16 @@ function MoviesList(props) {
   let filteredMovies = movies;
 
   if (visibilityFilter !== '') {
-    filteredMovies = movies.filter(m => m.title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+    filteredMovies = movies.filter(m => m.Title.includes(visibilityFilter));
   }
 
-  if (!movies) return <div className='main-view' />;
+  if (!movies) return <div className="main-view" />;
 
-  return <div className='movies-list'>
-    <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-    {filteredMovies.map(m => <MovieCard key={m.id} movie={m} />)}
-  </div>;
+  return <Container className="movies-list">
+    <VisibilityFilterInput visibilityFilter={visibilityFilter} /><div className="movies-list grid">{
+      filteredMovies.map(m => <MovieCard key={m._id} movie={m} />)} </div></Container>
+
 }
+
 
 export default connect(mapStateToProps)(MoviesList);
